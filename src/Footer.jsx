306d31e974f41953010c9e2f48ae66e62a6f6715
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-07-09 14:41:17 +0530

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,7 @@ import './index.css'
 
 function Footer() {
     return (
+        <></>
         // <footer>
         //     <div class="container center-content">
         //         <h5 class="center-content">Made<span class="dot"></span> <a href="https://github.com/Kachavi/College-site-clone" target="_blank">view source &nbsp;
