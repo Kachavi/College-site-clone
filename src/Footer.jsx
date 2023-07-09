1bd8155d5fd9b8b906e3e46460fbd30606973353
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-07-09 14:39:22 +0530

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,13 +2,13 @@ import './index.css'
 
 function Footer() {
     return (
-        <footer>
-            <div class="container center-content">
-                <h5 class="center-content">Made<span class="dot"></span> <a href="https://github.com/Kachavi/College-site-clone" target="_blank">view source &nbsp;
-                    <img class="github-img" src="/github-mark.png" alt="" />
-                </a></h5>
-            </div>
-        </footer>
+        // <footer>
+        //     <div class="container center-content">
+        //         <h5 class="center-content">Made<span class="dot"></span> <a href="https://github.com/Kachavi/College-site-clone" target="_blank">view source &nbsp;
+        //             <img class="github-img" src="/github-mark.png" alt="" />
+        //         </a></h5>
+        //     </div>
+        // </footer>
     )
 }
 
